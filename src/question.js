54e51e816309f796b579fb2a2a1d9dcfbc91428d
ecemonkol <ecemonkol@gmail.com>
2024-04-03 16:04:29 +0200

    feat: add constructor to questions class

--- src/question.js
+++ src/question.js
@@ -1,7 +1,8 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 }
