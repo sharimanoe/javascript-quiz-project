bf204071ef482803aad565ec14dd5552fc2b06df
ecemonkol <ecemonkol@gmail.com>
2024-04-03 17:09:25 +0200

    feat: organize data and functionality

--- src/question.js
+++ src/question.js
@@ -5,4 +5,9 @@ class Question {
     this.answer = answer;
     this.difficulty = difficulty;
   }
+
+  shuffleChoices() {
+    let mixedArray = this.choices.sort(() => Math.random() - 0.5);
+    return mixedArray;
+  }
 }
